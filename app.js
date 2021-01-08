a92a9176fe823db6e146bd4f447a53c375da2f99
Mamady Sillah <supurpisces310@msn.com>
2021-01-08 13:43:09 -0500

    added a filter function to search fruits

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ var app = angular.module('angularApp', []);
 app.controller("mainctrl", function(){ 
     this.hello = "world";
 
-    this.fruits = ['apples', 'oranges', 'grapes']
+    this.fruits = ['apples','oranges', 'grapes']
+
+    this.alertMe = function() {
+        alert('button pressed');
+    };
 });
